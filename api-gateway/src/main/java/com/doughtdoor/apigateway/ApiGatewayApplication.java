5d2aa035df4e92d64412b6156e91ed9eb34afafb
alitasdln <alitasdelen@ogrenci.karabuk.edu.tr>
2023-12-15 20:12:13 +0300

    Configured API gateway

--- api-gateway/src/main/java/com/doughtdoor/apigateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/doughtdoor/apigateway/ApiGatewayApplication.java
@@ -2,12 +2,19 @@ package com.doughtdoor.apigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 
 @SpringBootApplication
 public class ApiGatewayApplication {
+    public RouteLocator customRouteLocator (RouteLocatorBuilder builder) {
 
-    public static void main(String[] args) {
-        SpringApplication.run(ApiGatewayApplication.class, args);
+        return builder.routes().
+                route("order-management", r -> r.path("/orders/**")
+                    .uri("http://localhost:8088"))
+                .route("user-management", r -> r.path("/users/**")
+                    .uri("http://localhost:8888"))
+                .build();
     }
 
 }
